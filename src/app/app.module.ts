e0b72ac2b32e28f9ab81ebe7916c88dddf88034f
ErickChamorro <eschamorro97@misena.edu.co>
2018-11-10 08:57:56 -0500

    implementacion de componentes de material en el modulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, RouteReuseStrategy } from '@angular/router';
 
+// COPONENTE DE RUTAS
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// PAGINAS Y SUBPAGINAS
 import { HomeComponent } from './components/home/home.component';
 import { SharedComponent } from './shared/shared.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { BodyComponent } from './components/home/body/body.component';
-import { RouterModule, RouteReuseStrategy } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ErrorPageComponent } from './components/error-page/error-page.component';
 import { FourCeroFourComponent } from './components/error-page/four-cero-four/four-cero-four.component';
 import { FiveHundredComponent } from './components/error-page/five-hundred/five-hundred.component';
+
+// componentes de material (boton, forms e inputs)
+import {
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -28,9 +39,21 @@ import { FiveHundredComponent } from './components/error-page/five-hundred/five-
   ],
   imports: [
     BrowserModule,
+
+    // material buttons, forms, inputs
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+
     AppRoutingModule,
     BrowserAnimationsModule,
     RouterModule
+  ],
+  exports: [
+    // material buttons, forms, inputs
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
